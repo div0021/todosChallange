ada138732dbeaa789da591248c5e5d548a6aa6c7
div0021 <div2wk@gmail.com>
2023-06-25 23:03:15 +0530

    minor improvements

--- src/myComponents/AddTodos.tsx
+++ src/myComponents/AddTodos.tsx
@@ -15,13 +15,20 @@ const AddTodos:React.FC<Props> = ({addTodos}) => {
     const [control,setControl] = useState<string>('');
 
     const handleChange = (e:ChangeEvent<HTMLInputElement>) =>{
+      
         setControl(e.target.value);
     }
   return (
     <div className="flex w-full max-w-full items-center space-x-10">
     <Input type="text" placeholder="add details" value={control} onChange={handleChange}/>
-    <Button type="submit"  className="px-8" onClick={()=>{addTodos({text:control,completed:false})
-         setControl('')
+    <Button type="submit"  className="px-8" onClick={()=>{
+      if(control===''){
+        alert('please enter a todo')
+      }else{
+        addTodos({text:control,completed:false})
+        setControl('')
+      }
+      
        
 }}>Add</Button>
   </div>  )
